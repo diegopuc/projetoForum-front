c4f64bcaeff5b3bf398e09ef891045451c02b4e1
Prof. Diego <160940877+diegopuc@users.noreply.github.com>
2024-06-23 18:23:11 -0300

    Alterações configuração CORS - Testes.

--- src/service/http.service.ts
+++ src/service/http.service.ts
@@ -1,10 +1,11 @@
 import axios, { type AxiosInstance } from 'axios';
 
-//const baseURL = 'http://localhost:3000/api';
+// const baseURL = 'http://localhost:3000/api';
 const baseURL = 'https://main.d2stv5kwgtxo8r.amplifyapp.com/api';
 
 const api: AxiosInstance = axios.create({
   baseURL,
+  withCredentials: true, // para enviar cookies e credenciais
 });
 
 export default api;
