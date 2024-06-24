973ce98d822d32a5f1d7ff949d75aee76a1db486
Prof. Diego <160940877+diegopuc@users.noreply.github.com>
2024-06-24 17:01:20 -0300

    Teste Sincronização nuvem.

--- src/service/http.service.ts
+++ src/service/http.service.ts
@@ -3,6 +3,8 @@ import axios, { type AxiosInstance } from 'axios';
  //const baseURL = 'http://localhost:3000/api';
 const baseURL = 'main.d2stv5kwgtxo8r.amplifyapp.com/api';
 
+//+
+
 const api: AxiosInstance = axios.create({
   baseURL,
   //withCredentials: true, // para enviar cookies e credenciais
