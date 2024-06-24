5fbea798973d49846f14e538d847a77303cfb29a
Fábio Rodrigo de Souza <fabiors@pucpcaldas.br>
2024-06-24 17:07:22 -0300

    Altera a url da api

--- src/service/http.service.ts
+++ src/service/http.service.ts
@@ -1,7 +1,7 @@
 import axios, { type AxiosInstance } from 'axios';
 
  //const baseURL = 'http://localhost:3000/api';
-const baseURL = 'main.d2stv5kwgtxo8r.amplifyapp.com/api';
+const baseURL = 'https://main.d2stv5kwgtxo8r.amplifyapp.com/';
 
 const api: AxiosInstance = axios.create({
   baseURL,
