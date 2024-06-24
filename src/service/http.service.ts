0094811cd3ec72122ab11a4ca812b1e42fa72e9b
Prof. Diego <160940877+diegopuc@users.noreply.github.com>
2024-06-24 14:53:56 -0300

    Alteração link backend

--- src/service/http.service.ts
+++ src/service/http.service.ts
@@ -5,7 +5,7 @@ const baseURL = 'main.d2zml6m6uc2eec.amplifyapp.com/api';
 
 const api: AxiosInstance = axios.create({
   baseURL,
-  //withCredentials: true, // para enviar cookies e credenciais
+  withCredentials: true, // para enviar cookies e credenciais
 });
 
 export default api;
