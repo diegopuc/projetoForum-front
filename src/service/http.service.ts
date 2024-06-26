54070d8db0d60d834cac9365dc803f92f7458b2a
Prof. Diego <160940877+diegopuc@users.noreply.github.com>
2024-06-26 16:33:29 -0300

    teste com servidor faria

--- src/service/http.service.ts
+++ src/service/http.service.ts
@@ -1,7 +1,7 @@
 import axios, { type AxiosInstance } from 'axios';
 
  //const baseURL = 'http://localhost:3000/api';
-const baseURL = 'https://projetoforum-back.onrender.com/api';
+const baseURL = '38.242.137.190:3000/api';
 
 //+
 
